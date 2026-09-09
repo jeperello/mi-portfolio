ae1bdc0ce32e4f694856cc8384175db2500b7a70
jeperello <jorgeperello2020@gmail.com>
2026-09-09 08:46:24 -0300

    feat: move expierence word button

--- src/app/shared/navbar.component.ts
+++ src/app/shared/navbar.component.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { RouterModule } from '@angular/router';
 import { AnalyticsDirective } from './analytics.directive';
 import { ThemeService } from '../core/services/theme.service';
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle';
@@ -14,73 +13,7 @@ import { ThemeToggleComponent } from './theme-toggle/theme-toggle';
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   public readonly themeService = inject(ThemeService);
-  private readonly router = inject(Router);
-  public showBlogTooltip = signal(false);
   public isMenuOpen = signal(false);
-
-  private tooltipOpenTimer?: ReturnType<typeof setTimeout>;
-  private tooltipHideTimer?: ReturnType<typeof setTimeout>;
-  private hasShownBlogTooltip = false;
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
-      .subscribe((event) => {
-        const isProjectRoot = event.urlAfterRedirects === '/';
-
-        if (!isProjectRoot) {
-          this.clearTooltipTimers();
-          this.showBlogTooltip.set(false);
-          return;
-        }
-
-        this.scheduleBlogTooltip();
-      });
-
-    if (this.router.url === '/') {
-      this.scheduleBlogTooltip();
-    } else {
-      this.clearTooltipTimers();
-      this.showBlogTooltip.set(false);
-    }
-  }
-
-  private clearTooltipTimers(): void {
-    if (this.tooltipOpenTimer) {
-      clearTimeout(this.tooltipOpenTimer);
-      this.tooltipOpenTimer = undefined;
-    }
-
-    if (this.tooltipHideTimer) {
-      clearTimeout(this.tooltipHideTimer);
-      this.tooltipHideTimer = undefined;
-    }
-  }
-
-  private scheduleBlogTooltip(): void {
-    if (this.hasShownBlogTooltip) return;
-
-    this.clearTooltipTimers();
-    this.showBlogTooltip.set(false);
-
-    this.tooltipOpenTimer = setTimeout(() => {
-      this.hasShownBlogTooltip = true;
-      this.showBlogTooltip.set(true);
-
-      this.tooltipHideTimer = setTimeout(() => {
-        if (this.showBlogTooltip()) {
-          this.showBlogTooltip.set(false);
-        }
-      }, 9000);
-    }, 2500);
-  }
-
-  hideTooltip(): void {
-    this.clearTooltipTimers();
-    if (this.showBlogTooltip()) {
-      this.showBlogTooltip.set(false);
-    }
-  }
 }
